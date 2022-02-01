7670745cbf2a51cfb9d0d2b126123bb1a878d9fb
Valentine9594 <vtine95@gmail.com>
2022-02-01 19:18:09 +0530

    FirstViewModel created.

---
+++ NeoStore/Constants/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  NeoStore
+//
+//  Created by neosoft on 01/02/22.
+//
+
+import Foundation
---
+++ NeoStore/ViewModel/FirstViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FirstViewModel.swift
+//  NeoStore
+//
+//  Created by neosoft on 01/02/22.
+//
+
+import Foundation
