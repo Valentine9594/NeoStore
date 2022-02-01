5577aaa140465c5d3a3779ded0e85e484345d7e7
Valentine9594 <vtine95@gmail.com>
2022-02-01 16:36:07 +0530

    Adding Files.

---
+++ NeoStore/Model/Services/APIManager.swift
@@ -0,0 +1,20 @@
+//
+//  APIManager.swift
+//  NeoStore
+//
+//  Created by neosoft on 01/02/22.
+//
+
+import Foundation
+
+class APIManager{
+    
+    func requestURL(){
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: URL(string: "")!)
+//        request.httpMethod = "GET"
+        
+        
+    }
+}
---
+++ NeoStore/Model/Services/APIServices.swift
@@ -0,0 +1,16 @@
+//
+//  APIServices.swift
+//  NeoStore
+//
+//  Created by neosoft on 01/02/22.
+//
+
+import Foundation
+
+var baseURL = "http://staging.php-dev.in:8844/trainingapp"
+
+enum APIServices{
+    public struct subDomain{
+        static let apiDomain = "/api/users"
+    }
+}
