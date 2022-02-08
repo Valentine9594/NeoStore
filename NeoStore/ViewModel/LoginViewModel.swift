b1222ea535bc84d8dcb86e24ab448b1c6b8877d5
Valentine9594 <vtine95@gmail.com>
2022-02-08 15:07:34 +0530

    Login viewmodel and Register viewmodel working.

--- NeoStore/ViewModel/LoginViewModel.swift
+++ NeoStore/ViewModel/LoginViewModel.swift
@@ -16,17 +16,6 @@ class LoginViewModel: LoginViewModelType{
         UserService.userLogIn(username: userName, password: userPassword) { response in
             switch response{
                 case .success(let data):
-                    if let content = data as? Data{
-                        do {
-                            let json = try JSONSerialization.jsonObject(with: content, options: .mutableContainers)
-                            print(json)
-                        } catch let error {
-                            debugPrint(error.localizedDescription)
-                        }
-                    }
-                    else{
-                        print(CustomErrors.ResponseDataNil.localizedDescription)
-                    }
                 print(data)
                 case .failure(let error):
                     debugPrint(error.localizedDescription)
