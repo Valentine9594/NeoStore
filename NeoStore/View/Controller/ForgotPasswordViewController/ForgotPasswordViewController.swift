b9baf3fb87ab62f972182da91254b0813088072c
Valentine9594 <vtine95@gmail.com>
2022-02-09 18:32:02 +0530

    Forgot Password View controller created.

--- NeoStore/View/Controller/ForgotPasswordViewController/ForgotPasswordViewController.swift
+++ NeoStore/View/Controller/ForgotPasswordViewController/ForgotPasswordViewController.swift
@@ -9,13 +9,25 @@ import UIKit
 
 class ForgotPasswordViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    private func setupUI(){
+        self.view.backgroundColor = UIColor.appRed
+        
+        setTextField(textfield: usernameTextField, image: UIImage(named: textFieldIcons.usernameIcon.rawValue))
+        
+        sendButton.layer.cornerRadius = 7
+    }
 
-
-
+    @IBAction func sendButtonAction(_ sender: UIButton) {
+        debugPrint("Clicking Send Button!")
+    }
+    
 }
---
+++ NeoStore/ViewModel/ForgotPasswordViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ForgotPasswordViewModel.swift
+//  NeoStore
+//
+//  Created by neosoft on 09/02/22.
+//
+
+import Foundation
+
