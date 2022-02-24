89abd689c2bf5851f6535b9bfe77f3b7fa793be7
Valentine9594 <vtine95@gmail.com>
2022-02-24 16:17:34 +0530

    added iboutlets to product detail viewcontroller and added more ui elements.

--- NeoStore/View/Controller/ProductDetailedViewController/ProductDetailedViewController.swift
+++ NeoStore/View/Controller/ProductDetailedViewController/ProductDetailedViewController.swift
@@ -8,13 +8,73 @@
 import UIKit
 
 class ProductDetailedViewController: UIViewController {
-
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productCategory: UILabel!
+    @IBOutlet weak var productType: UILabel!
+    @IBOutlet weak var ratingStar1: UIImageView!
+    @IBOutlet weak var ratingStar2: UIImageView!
+    @IBOutlet weak var ratingStar3: UIImageView!
+    @IBOutlet weak var ratingStar4: UIImageView!
+    @IBOutlet weak var ratingStar5: UIImageView!
+    
+    @IBOutlet weak var bodyContainerView:
+        UIView!
+    @IBOutlet weak var descriptionView: UIView!
+    @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var scrollContentView: UIView!
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productImagesCollectionView: UICollectionView!
+    
+    
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var buyNowButton: UIButton!
+    @IBOutlet weak var rateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupUI()
+        setupNavigationBar()
     }
 
+    private func setupUI(){
+        let cornerRadius: CGFloat = 7
+        buyNowButton.layer.cornerRadius = cornerRadius
+        rateButton.layer.cornerRadius = cornerRadius
+        bodyContainerView.layer.cornerRadius = cornerRadius
+        descriptionView.layer.cornerRadius = cornerRadius
+        imageContainerView.layer.cornerRadius = cornerRadius
+        scrollContentView.layer.cornerRadius = cornerRadius
+        
+        self.productName.text = "Product Name?"
+        
+        
+    }
 
+    private func setupNavigationBar(){
+//        function to setup navigation bar
+        let navigationBar = self.navigationController?.navigationBar
+        navigationBar?.barTintColor = UIColor.appRed
+        navigationBar?.tintColor = UIColor.white
+        navigationBar?.isTranslucent = appAnimation
+        navigationBar?.barStyle = .black
+        navigationBar?.titleTextAttributes = [.foregroundColor: UIColor.white, .font: UIFont(name: "iCiel Gotham Medium", size: 23.0)!]
+        
+        navigationItem.title = self.productName.text
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(popToPreviousViewController))
+    }
+    
+    @IBAction func clickedShareButton(_ sender: UIButton) {
+        debugPrint("Tapped to Share!")
+    }
+    
+    
+    @objc func popToPreviousViewController() -> Void{
+        self.navigationController?.popViewController(animated: appAnimation)
+    }
+    
 
 }
